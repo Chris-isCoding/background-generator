de65922509525fb2d9ae468bcff021639e6fafdf
Chris K <Chris-SourceCode@outlook.com>
2021-07-14 23:59:08 +0100

    [background_generator]: add core functionality using DOM manipulation

---
+++ script.js
@@ -0,0 +1,12 @@
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const body = document.getElementById('gradient');
+
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = `${body.style.backgroundImage};`;
+}
+
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
